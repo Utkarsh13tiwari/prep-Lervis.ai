1acc1acbe63f5ef7e384e31874dcaefc04b21e34
UTKARSH TIWARI <93029055+Utkarsh13tiwari@users.noreply.github.com>
2024-09-26 22:58:03 +0530

    Update pdf_rag.py

--- pdf_rag.py
+++ pdf_rag.py
@@ -29,7 +29,7 @@ def pdf_rag(file_path, user_input):
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     splits = text_splitter.split_documents(docs)
-    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings(openai_api_key = openai))
 
     retriever = vectorstore.as_retriever()
 
