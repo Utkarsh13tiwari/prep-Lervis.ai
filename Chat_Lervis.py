9f02cddbe5654deb53111916f3c73140501963e9
UTKARSH TIWARI <93029055+Utkarsh13tiwari@users.noreply.github.com>
2024-09-27 00:49:10 +0530

    Update Chat_Lervis.py

--- Chat_Lervis.py
+++ Chat_Lervis.py
@@ -448,8 +448,8 @@ if user_input:
         if selected_option == 'Job Description':
             agent_response = pdf_rag(job_des, user_input=user_input)
         else:
-            agent_response = webrag(link, user_input)
-	    related_links = google_search(user_input, link)
+		agent_response = webrag(link, user_input)
+		related_links = google_search(user_input, link)
 
     else:
         with get_openai_callback():
@@ -458,7 +458,6 @@ if user_input:
 
         message = {'user': user_input, 'AI': agent_response}
         st.session_state.chat_history.append(message)
-
     	related_links = google_search(user_input)
     
     # Display the response
