63cda1944e8b9c49127fea036266528e2ff9bc5e
UTKARSH TIWARI <93029055+Utkarsh13tiwari@users.noreply.github.com>
2024-09-27 02:02:11 +0530

    Update rag_app.py

--- rag_app.py
+++ rag_app.py
@@ -51,7 +51,7 @@ openai = st.secrets.db_credentials.openai
 nvidia = st.secrets.db_credentials.nvidia
 
 langchain_api = st.secrets.db_credentials.langchain_api
-LANGCHAIN_TRACING_V2=true
+LANGCHAIN_TRACING_V2= "true"
 LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
 LANGCHAIN_API_KEY=langchain_api
 LANGCHAIN_PROJECT="prep-w-lervis"
