725a59885ff29320edd163e16658f17de2149169
UTKARSH TIWARI <93029055+Utkarsh13tiwari@users.noreply.github.com>
2024-10-01 01:25:40 +0530

    Update Voice_Lervis.py

--- pages/Voice_Lervis.py
+++ pages/Voice_Lervis.py
@@ -9,7 +9,7 @@ from langchain_openai import ChatOpenAI
 from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
 from langchain_community.tools.tavily_search.tool import TavilySearchResults
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.agents import AgentExecutor
+from langchain.agents import AgentExecutor,create_tool_calling_agent
 from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
 from langchain_core.utils.function_calling import format_tool_to_openai_function
 from langchain.agents.format_scratchpad.openai_tools import format_to_openai_tool_messages
@@ -109,6 +109,7 @@ llm = ChatGroq(
     max_tokens=None,
     timeout=None,
     max_retries=2,
+    api_key=GROQ_API_KEY,
     # other params...
 )
 
