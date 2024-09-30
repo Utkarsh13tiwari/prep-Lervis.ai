f73006e0455b23fb8224b27126bec38e367b5294
UTKARSH TIWARI <93029055+Utkarsh13tiwari@users.noreply.github.com>
2024-10-01 01:20:39 +0530

    Update Voice_Lervis.py

--- pages/Voice_Lervis.py
+++ pages/Voice_Lervis.py
@@ -31,12 +31,15 @@ import threading
 import os
 from streamlit_mic_recorder import mic_recorder, speech_to_text
 import streamlit_mic_recorder
+from langchain_groq import ChatGroq
+from langchain.chains import LLMChain
 
 # Set environment variables
 openai = st.secrets.db_credentials.openai 
 tavily = st.secrets.db_credentials.tavily
 api = st.secrets.db_credentials.api
 search_engine = st.secrets.db_credentials.search_engine
+GROQ_API_KEY = st.secrets.db_credentials.GROQ_API_KEY
 
 langchain_api = st.secrets.db_credentials.langchain_api
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
@@ -83,39 +86,40 @@ with col3:
         """, unsafe_allow_html=True)
 	    
 # Initialize the agent and memory
-openai_model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.5, openai_api_key=openai)
 search = TavilySearchAPIWrapper(tavily_api_key=tavily)
 tavily_tool = TavilySearchResults(api_wrapper=search)
 tools = [tavily_tool]
 
-# Format tools to OpenAI functions
-functions = [format_tool_to_openai_function(t) for t in tools]
-llm_with_tools = openai_model.bind_functions(functions=functions)
-
 memory = ConversationBufferWindowMemory(return_messages=True, memory_key='chat_history', input_key='input', k=10)
 prompt_template = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are an AI interviewer. Based on the job description provided, create relevant interview questions. Ask user, questions one by one, and then let user reply after every asked question first. Make sure you build up upon the user previouse answers"),
-        MessagesPlaceholder(variable_name="agent_scratchpad"),
+        ("system", "You are an AI interviewer. Based on the job description provided,"
+        "create relevant interview questions. Ask user, questions one by one, one each time and then let user reply after every asked question first."
+        "Dont ask multiple questions at a time. Make sure you build up upon the user previouse answers."
+        "Apart from the interview related or question you will not answer anything."),
         ("user", "{JD}"),
         MessagesPlaceholder(variable_name="chat_history"),
         ("user", "{input}")
     ]
 )
 
-agent = (
-    {
-        "JD": lambda x: x["JD"],
-        "agent_scratchpad": lambda x: format_to_openai_tool_messages(x["intermediate_steps"]),
-        "input": lambda x: x["input"],
-        "chat_history": lambda x: x["chat_history"]
-    }
-    | prompt_template
-    | llm_with_tools
-    | OpenAIToolsAgentOutputParser()
+llm = ChatGroq(
+    model="mixtral-8x7b-32768",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
 )
 
-agent_executor = AgentExecutor(agent=agent, tools=tools, memory=memory, verbose=True)
+#agent = create_tool_calling_agent(llm, tools, prompt_template)
+agent_executor = LLMChain(
+    llm=llm,
+    prompt=prompt_template,
+    verbose=True,
+    memory=memory
+)
+#agent_executor = AgentExecutor(agent=agent, tools=tools, memory=memory, verbose=True, stream_runnable=False)
 #--------------------------------------------------------------------------------------------------------------------------
 
 
@@ -130,7 +134,9 @@ if 'chat_history' not in st.session_state:
     st.session_state.chat_history = []
 else:
     for message in st.session_state.chat_history:
-        memory.save_context({'input':message['user']},{'outputs':message['AI']})
+        memory.chat_memory.add_user_message(message['user'])
+        memory.chat_memory.add_ai_message(message['AI'])
+        #memory.save_context({'input':message['user']},{'outputs':message['AI']})
 
 #--------------------------------------------------------------------------------------------------------------------------
 
@@ -155,11 +161,8 @@ def text_to_speech(text):
     wav_audio = asyncio.run(text_to_speech_edge(text))
     st.audio(wav_audio, autoplay=True ,format='audio/wav') 
     return
-#---------------------------------------------------------------------------------------------------------------------------
 
-# Streamlit UI
 #st.set_page_config(page_title="Virtual AI Interviewer", layout="wide")
-
 row1 = st.columns([1, 15, 1])
 row2 = st.columns([1, 15, 1])
 #col1, col2, col3 = st.columns([1, 3, 1])
@@ -167,22 +170,27 @@ container = st.container(border=True)
 with row1[1]:
     st.title("Virtual AI Interviewer")
 
+    # User inputs the job description
     JD = st.text_area("Enter Job Description (JD)")
 
+    # Store the state for the first question
     if "first_question_asked" not in st.session_state:
         st.session_state.first_question_asked = False
 
+    # Button to start the interview
     if st.button("Start Interview"):
         if JD and not st.session_state.first_question_asked:
             # Generate the first question
             with st.container():
                 with st.spinner("Working......"):
-                    question = agent_executor.invoke({"JD": JD,"input": ""})['output']
+                    question = agent_executor.invoke({"JD": JD,"input": "", "chat_history": st.session_state.chat_history})
+                question = question['text']
                 st.session_state.first_question = question
                 st.write(f"AI Interviewer: {question}")
-            #message = {'user': JD, 'AI': question}
+                message = {'user': JD, 'AI': question}
+                #print(memory)
             #memory.save_context({'JD':message['user']},{'outputs':message['AI']})
-            #st.session_state.chat_history.append(message)
+                st.session_state.chat_history.append(message)
             text_to_speech(question)
             st.session_state.first_question_asked = True
         elif not JD:
@@ -191,6 +199,7 @@ with row1[1]:
     if st.session_state.first_question_asked:
         st.write("Start replying to the Questions by clicking Start-recording button:")
 
+        # Fixed button for speech-to-text
         with row2[1]:
             text = streamlit_mic_recorder.speech_to_text(language='en', use_container_width=False, just_once=True, key='STT')
 
@@ -204,8 +213,10 @@ with row1[1]:
             if text is not None:
                 user_response = text
                 with st.spinner("Working......"):
-                    next_question = agent_executor.invoke({"JD": "", "input": user_response, "chat_history": st.session_state.chat_history})['output']
+                    next_question = agent_executor.invoke({"JD": JD, "input": user_response, "chat_history": st.session_state.chat_history})
+                    next_question = next_question['text']
                 message = {'user': user_response, 'AI': next_question}
                 st.session_state.chat_history.append(message)
                 st.write(f"AI Interviewer: {next_question}")
                 text_to_speech(next_question)
+
