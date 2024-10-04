d4a2bffdb64875a0050eba4cd2e049a51aa4795d
UTKARSH TIWARI <93029055+Utkarsh13tiwari@users.noreply.github.com>
2024-10-04 20:24:11 +0530

    Update Queriz.py

--- pages/Queriz.py
+++ pages/Queriz.py
@@ -1,6 +1,7 @@
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import chromadb
 from langchain.schema import Document
 from langchain.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
